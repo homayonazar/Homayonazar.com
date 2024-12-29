f86e712ea3ee158276b9429b9ce411584d200c5e
homayonazar <amirrezaabdollahi01@gmail.com>
2024-12-29 17:35:36 +0300

    Add automatic day/night mode based on user's system time

--- assets/js/style-switcher.js
+++ assets/js/style-switcher.js
@@ -1,57 +1,48 @@
-/* ------toggle  style switcher ------- */
-
+/* ------ Toggle Style Switcher ------- */
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
-})
-
+});
 
 /* Hide Style Switcher */
 window.addEventListener("scroll", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
     }
-})
+});
 
-/* Toggle style Switcher */
-
+/* Toggle Style Colors */
 const alternateStyles = document.querySelectorAll(".alternative-style");
-function setActiveStyle(color) 
-{
+function setActiveStyle(color) {
     alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title")) {
+        if (color === style.getAttribute("title")) {
             style.removeAttribute("disabled");
-        }
-        else {
+        } else {
             style.setAttribute("disabled", "true");
         }
     });
 }
 
-/* Theme light - Night Mode */
-
+/* Theme Light - Night Mode */
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
 });
 
-
-
+/* Load Theme on Window Load */
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark"))
-    {
+    // Detect system theme
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDark) {
+        document.body.classList.add("dark");
+        dayNight.querySelector("i").classList.remove("fa-moon");
         dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else
-    {
+    } else {
+        document.body.classList.remove("dark");
+        dayNight.querySelector("i").classList.remove("fa-sun");
         dayNight.querySelector("i").classList.add("fa-moon");
     }
-
-
-
-
-
-})
+});
